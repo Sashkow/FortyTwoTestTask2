90ae9b503b7f4d4a0e5cb28af10ea490f074e063
Sashkow <lykhenko.olexandr@gmail.com>
2015-01-13 12:26:40 +0200

    ticket:1 minimal mainpage_view test red

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 
 def main_page(request):
-	return render(request,"hello/base.html",{})
+	pass
+	# return render(request,"hello/base.html",{})
